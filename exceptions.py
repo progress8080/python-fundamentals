7ebcf5b28868d64073c2bbfb9691fb0f60ebe93a
progress8080 <code.master961223@gmail.com>
2026-03-13 15:19:42 +0100

    feat(m1-exceptions): simple practive with def and exceptions

---
+++ exceptions.py
@@ -0,0 +1,13 @@
+def divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return "Error: Division by zero"
+
+print(divide(1, 0))
+
+# python exception handling
+try:
+    print(divide(1, 0))
+except ZeroDivisionError:
+    print("Error: Division by zero")
