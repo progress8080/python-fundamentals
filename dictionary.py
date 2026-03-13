173e0456c75fc342480faff0239a2b9c39c9e071
progress8080 <code.master961223@gmail.com>
2026-03-13 15:03:58 +0100

    feat(m1-list, dict): simple practice of list and dict

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+person = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+
+print(person)
+
+person["age"] = 31
+print(person)
+
+person["country"] = "USA"
+print(person)
+
+person["nationality"] = "American"
+print(person)
---
+++ list.py
@@ -0,0 +1,13 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+fruits.append("orange")
+print(fruits)
+
+fruits.remove("banana")
+print(fruits)
+
+fruits.pop()
+print(fruits)
+
+fruits.clear()
